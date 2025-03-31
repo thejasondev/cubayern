adc71193be6ac1405f87733befbe82a2702903ea
OsvaFernandez <osvanaranjo96@gmail.com>
2025-03-31 16:43:26 -0400

    add react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@astrojs/tailwind";
+import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwindcss()],
+  integrations: [tailwindcss(), react()],
   site: 'http://localhost:4321',
   base: '/',
 });
